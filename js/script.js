be5370d623fbe70e67e6bade72f3593dd3cf38dd
Juliana Duarte <Juliana Duarte>
2016-11-13 20:43:39 -0200

    fist

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).on("pagecreate", "#estilo", function(){
+    $(".animateMe .ui-collapsible-heading-toggle").on("click", function (e) { 
+        var current = $(this).closest(".ui-collapsible");             
+        if (current.hasClass("ui-collapsible-collapsed")) {
+            $(".ui-collapsible").not(".ui-collapsible-collapsed").find(".ui-collapsible-heading-toggle").click();
+            $(".ui-collapsible-content", current).slideDown(300);
+        } else {
+            $(".ui-collapsible-content", current).slideUp(300);
+        }
+    });
+});
